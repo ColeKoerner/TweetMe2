97dc595bc27589b586899f6d90504e59b24ecd0c
Cole Koerner <colekoerner.ck@gmail.com>
2021-06-18 07:32:38 -0500

    update

---
+++ todo.md
@@ -0,0 +1,14 @@
+1. Tweets
+    -> Creating
+        -> Text
+        -> Image -> Media Storage Server
+    -> Delete
+    -> Retweeting
+    -> Liking
+
+2. Users
+    -> Register
+    -> Login
+    -> Logout
+    -> Profile
+        -> Image?
--- tweetme2/settings.py
+++ tweetme2/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'yjyzr)awrwbf5$zs7!he)xvg_n=d3(b=w8goty0dav$sroh5!-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.mydomain.com']
 
 
 # Application definition
