cfa699e380e2811545dc1170d743ccba05f15b0e
cole.koerner <cole.koerner@invista.com>
2021-09-29 12:34:00 -0500

    lunch 9/29

--- tweets/views.py
+++ tweets/views.py
@@ -47,7 +47,7 @@ def tweet_action_view(request, *args, **kwargs):
     ID is required
     Action options are: like, unlike, retweet
     '''
-    serializer = TweetActionSerializer(data=request.POST)
+    serializer = TweetActionSerializer(data=request.data)
     if serializer.is_valid(raise_exception=True):
         data = serializer.validated_data
         tweet_id = data.get("id")
@@ -59,12 +59,14 @@ def tweet_action_view(request, *args, **kwargs):
         obj = qs.first()
         if action == "like":
             obj.likes.add(request.user)
+            serializer = TweetSerializer(obj)
+            return Response(serializer.data, status=200)
         elif action == "unlike":
             obj.likes.remove(request.user)
         elif action == "retweet":
             # to do
             pass
-        return Response({"messge": "Tweet removed"}, status=200)
+        return Response({}, status=200)
 
 @api_view(['DELETE', 'POST'])
 @permission_classes([IsAuthenticated])
