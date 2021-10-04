da0d2b8402a4c8e27b3cac27a80db4330a672c06
cole.koerner <cole.koerner@invista.com>
2021-10-04 15:22:43 -0500

    10/4 lunch

--- tweets/tests.py
+++ tweets/tests.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
+from rest_framework.test import APIClient
 
 from .models import Tweet
 # Create your tests here.
@@ -7,8 +8,28 @@ User = get_user_model()
 class TweetTestCase(TestCase):
     def setUp(self):
         self.user = User.objects.create_user(username='abc', password='somepassword')
-
+        Tweet.objects.create(content="my first tweet", user=self.user)
+        Tweet.objects.create(content="my second tweet", user=self.user)
+        Tweet.objects.create(content="my third tweet", user=self.user)
+        
     def test_tweet_created(self):
-        tweet_obj = Tweet.objects.create(content="my tweet", user=self.user)
-        self.assertEqual(tweet_obj.id, 1)
+        tweet_obj = Tweet.objects.create(content="my second tweet", user=self.user)
+        self.assertEqual(tweet_obj.id, 4)
         self.assertEqual(tweet_obj.user, self.user)
+
+    def get_client(self):
+        client = APIClient()
+        client.login(username=self.user, password='somepassword')
+        return client
+
+    def test_tweet_list(self):
+        client = self.get_client()
+        response = client.get("/api/tweets/")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.json()), 3)
+
+    def test_action_like(self):
+        client = self.get_client()
+        response = client.post("/api/tweets/action", {"id": 1, "action": "like"})
+        self.assertEqual(response.status_code, 200)
+        print(response.json)
