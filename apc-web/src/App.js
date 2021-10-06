3a2a5ea58c28386f998f881635d1f119420c716e
Cole Koerner <colekoerner.ck@gmail.com>
2021-10-05 22:15:33 -0500

    10/5 night

--- apc-web/src/App.js
+++ apc-web/src/App.js
@@ -1,51 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 
-function loadTweets(callback) {
-  const xhr = new XMLHttpRequest()
-  const method = "Get"
-  const url = "http://localhost:8000/api/tweets/"
-  const responseType = "json"
-  xhr.responseType = responseType
-  xhr.open(method, url)
-  xhr.onload = function() {
-      callback(xhr.response, xhr.status)
-  }
-  xhr.onerror = function (e) {
-    console.log(e)
-    callback({"message": "The request was an error"}, 400)
-  }
-  xhr.send()
-}
-
-function Tweet(props) {
-  const {tweet} = props
-  const className = props.className ? props.className : 'col-10 mx-auto col-md-6'
-  return <div className={className}>
-    <p>{tweet.id} - {tweet.content}</p>
-  </div>
-}
-
+import {TweetsList} from './tweets'
 
 function App() {
-  const [tweets, setTweets] = useState([{content: 123}])
-  
-
-  useEffect(() =>{
-    // do my lookup
-    const myCallback = (response, status) => {
-      console.log(response, status)
-      if(status === 200){
-        setTweets(response)
-      }
-      else {
-        alert("There was an error")
-      }
-    }
-    loadTweets(myCallback)
-  }, [])
-
   return (
     <div className="App">
       <header className="App-header">
@@ -53,11 +12,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>
-          {tweets.map((item, index)=>{
-            return <Tweet tweet={item} key={index}/>
-          })}
-        </p>
+        <div>
+          <TweetsList />
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
